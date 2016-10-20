a67b5f5fda236e11e4ad4fa16290453fe63203e3
alexandernzach <alex.97@live.com>
2016-10-20 20:53:48 +0100

    Add files via upload

---
+++ adding numbers.py
@@ -0,0 +1,16 @@
+print("Add two numbers together")
+
+# The program is asking for 2 numbers.
+
+Number1=int(input("Enter one number"))
+
+Number2=int(input("Enter one number"))
+
+# The numbers will be added together.
+
+sum=Number1+Number2
+
+#The sum of the two added numbers will be displayed here.
+
+print(sum)
+            
